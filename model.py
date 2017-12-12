修正softmax冗余的bug

model.py
@@ -37,10 +37,7 @@ class UnifiedFramework(nn.Module):
         self.context_repr_weight = nn.Linear(config['gcr_hidden_size'] * 4, config['feature_size'])
         self.path_repr_weight = nn.Linear(config['gpr_hidden_size'] * 2 + config['rpr_hidden_size'] * 2,
                                           config['feature_size'])
-        self.output = nn.Sequential(
-            nn.Linear(config['feature_size'], config['categories']),
-            nn.Softmax(dim=-1)
-        )
+        self.output = nn.Linear(config['feature_size'], config['categories'])
 
     def forward(self, word_seq, pos_seq, rel_pos, sent_len, token_index_path, depend_path,
                 rel_token_index_path, rel_depend_path, path_len, rel_path_len):

train.py
@@ -31,7 +31,7 @@ config = {
     'rpr_hidden_size': 200,
     'rpr_num_layers': 5,
     'feature_size': 200,
-    'drop_out': 0.1,
+    'drop_out': 0.3,
     'categories': max(trainloader.dataset.label2idx.values()) + 1
 }
 
@@ -44,7 +44,7 @@ if DEVICE_NO != -1:
 optimizer = torch.optim.Adagrad(uf.parameters(), lr=0.01)
 criteria = nn.CrossEntropyLoss(ignore_index=0)
 
-log_interval = 10
+log_interval = 20
 epochs = 10
 
 